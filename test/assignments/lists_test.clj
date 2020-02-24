5625e299dc9e2e94bf1be9150443b687609dfb36
Naveen Yellapu <naveen.y.aux@go-jek.com>
2020-02-25 01:51:45 +0530

    Add extra test to lists-test to test the behavior of map with uneven collection length

--- test/assignments/lists_test.clj
+++ test/assignments/lists_test.clj
@@ -8,5 +8,5 @@
       (is (= [1 2 3] (map' identity [1 2 3]))))
     (testing "addition with double coll"
       (is (= [5 7 9] (map' + [1 2 3] [4 5 6]))))
-    (testing "addition with double coll"
-      (is (= '(5 7 9) (map' + '(1 2 3) [4 5 6]))))))
+    (testing "addition with multi collections should only return sequence with length of the smallest collection"
+      (is (= '(6 9) (map' + '(1 2 3) [4 5 6 7] '(1 2)))))))
