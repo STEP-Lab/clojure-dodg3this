f0d60ee28fe95d25a3ccf4b5a0087aebc15934fa
Naveen Yellapu <naveen.y.aux@go-jek.com>
2020-02-25 00:08:52 +0530

    Implement Harischandra

--- test/assignments/conditions_test.clj
+++ test/assignments/conditions_test.clj
@@ -13,3 +13,11 @@
     (is (= 2 (informative-divide 4 2))))
   (testing "zero denominator"
     (is (= :infinite (informative-divide 3 0)))))
+
+(deftest harishchandra-says
+  (testing "the same if truthy"
+    (is (= 4 (harishchandra 4))))
+  (testing "nothing if false"
+    (is (nil? (harishchandra false))))
+  (testing "nothing if nil"
+    (is (nil? (harishchandra nil)))))
